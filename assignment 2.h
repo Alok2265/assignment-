d83b0b5a7ea0b6320efa17f7c26b5170a4cb0ca0
Alok2265 <akys2265@gmail.com>
2025-04-30 17:19:22 +0530

    Add files via upload

---
+++ assignment 2.h
@@ -0,0 +1,30 @@
+int factorial(int n);
+int evenodd(int l);
+int prime(int a);
+
+int evenodd(int l){
+    if(l%2==0){
+        return 0;
+    }
+    return 1;
+}
+
+int count(int a){
+    int b=0;
+    
+    for(int i =1;i<=a;i++){ 
+        b+=1;
+        }
+        return b;
+        
+    }     
+    
+
+
+int factorial(int n){
+    int prod=1;
+    for (int i =1;i<=n;i++){
+        prod*=i;
+    }
+    return prod;
+}
